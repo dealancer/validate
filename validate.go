c603f61cf6d657d395c35e21a10872b938883526
Vadym Myrgorod <vmyrgorod@crumptongroup.com>
2019-05-10 16:02:23 -0400

    Add Pointer and Struct support

--- validate.go
+++ validate.go
@@ -11,6 +11,7 @@ const (
 	tagMin      = "validate_min"
 	tagMax      = "validate_max"
 	tagNotEmpty = "validate_not_empty"
+	tagNotNil   = "validate_not_nil"
 )
 
 var (
@@ -23,43 +24,53 @@ func Validate(ptr interface{}) error {
 		return errInvalidType
 	}
 
-	v := reflect.ValueOf(ptr).Elem()
-	t := v.Type()
+	value := reflect.ValueOf(ptr).Elem()
+	typ := value.Type()
 
-	if t.Kind() != reflect.Struct {
+	if typ.Kind() != reflect.Struct {
 		return errInvalidType
 	}
 
-	for i := 0; i < t.NumField(); i++ {
-		if err := validateField(t.Field(i), v.Field(i)); err != nil {
+	for i := 0; i < typ.NumField(); i++ {
+		if err := validateField(value.Field(i), value.Field(i).Kind(), typ.Field(i).Name, typ.Field(i).Tag); err != nil {
 			return err
 		}
-
 	}
 
 	return nil
 }
 
-func validateField(f reflect.StructField, v reflect.Value) error {
-	switch v.Kind() {
+func validateField(value reflect.Value, kind reflect.Kind, name string, tag reflect.StructTag) error {
+	switch kind {
 	case reflect.String:
-		if notEmpty, err := strconv.ParseBool(f.Tag.Get(tagNotEmpty)); err == nil && notEmpty && v.String() == "" {
-			return errors.New(fmt.Sprint(f.Name, " must not be empty"))
+		if notEmpty, err := strconv.ParseBool(tag.Get(tagNotEmpty)); err == nil && notEmpty && value.String() == "" {
+			return errors.New(fmt.Sprint(name, " must not be empty"))
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if min, err := strconv.ParseInt(f.Tag.Get(tagMin), 10, 64); err == nil && v.Int() < min {
-			return errors.New(fmt.Sprint(f.Name, " must not be less than ", min))
+		if min, err := strconv.ParseInt(tag.Get(tagMin), 10, 64); err == nil && value.Int() < min {
+			return errors.New(fmt.Sprint(name, " must not be less than ", min))
 		}
-		if max, err := strconv.ParseInt(f.Tag.Get(tagMax), 10, 64); err == nil && v.Int() > max {
-			return errors.New(fmt.Sprint(f.Name, " must not be greater than ", max))
+		if max, err := strconv.ParseInt(tag.Get(tagMax), 10, 64); err == nil && value.Int() > max {
+			return errors.New(fmt.Sprint(name, " must not be greater than ", max))
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		if min, err := strconv.ParseUint(f.Tag.Get(tagMin), 10, 64); err == nil && v.Uint() < min {
-			return errors.New(fmt.Sprint(f.Name, " must not be less than ", min))
+		if min, err := strconv.ParseUint(tag.Get(tagMin), 10, 64); err == nil && value.Uint() < min {
+			return errors.New(fmt.Sprint(name, " must not be less than ", min))
 		}
-		if max, err := strconv.ParseUint(f.Tag.Get(tagMax), 10, 64); err == nil && v.Uint() > max {
-			return errors.New(fmt.Sprint(f.Name, " must not be greater than ", max))
+		if max, err := strconv.ParseUint(tag.Get(tagMax), 10, 64); err == nil && value.Uint() > max {
+			return errors.New(fmt.Sprint(name, " must not be greater than ", max))
 		}
+	case reflect.Slice:
+		if notEmpty, err := strconv.ParseBool(tag.Get(tagNotEmpty)); err == nil && notEmpty && value.Len() == 0 {
+			return errors.New(fmt.Sprint(name, " must not be empty"))
+		}
+	case reflect.Ptr:
+		if value.IsNil() {
+			if notNil, err := strconv.ParseBool(tag.Get(tagNotNil)); err == nil && notNil {
+				return errors.New(fmt.Sprint(name, " must not be nil"))
+			}
+		}
+		return validateField(value.Elem(), value.Elem().Kind(), name, tag)
 	}
 
 	return nil
