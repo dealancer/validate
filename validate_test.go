e5525147db28c152839ceb40db9f42bc15679c08
Vadym Myrgorod <vm386@georgetown.edu>
2019-05-11 17:00:18 -0400

    Add inverse tests and add min/max tests for strings and maps

--- validate_test.go
+++ validate_test.go
@@ -21,6 +21,22 @@ func TestMinTagForDuration(t *testing.T) {
 	}) {
 		t.Errorf("min tag does not validate for time.Duratuon")
 	}
+
+	if nil != Validate(struct {
+		field time.Duration `min:"0s"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for time.Duratuon")
+	}
+
+	if nil != Validate(struct {
+		field time.Duration `min:"-1s"`
+	}{
+		field: -time.Millisecond,
+	}) {
+		t.Errorf("min tag does not validate for time.Duratuon")
+	}
 }
 
 func TestMaxTagForDuration(t *testing.T) {
@@ -36,6 +52,22 @@ func TestMaxTagForDuration(t *testing.T) {
 		field time.Duration `max:"1s"`
 	}{
 		field: time.Minute,
+	}) {
+		t.Errorf("max tag does not validate for time.Duratuon")
+	}
+
+	if nil != Validate(struct {
+		field time.Duration `max:"0s"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for time.Duratuon")
+	}
+
+	if nil != Validate(struct {
+		field time.Duration `max:"1s"`
+	}{
+		field: time.Millisecond,
 	}) {
 		t.Errorf("max tag does not validate for time.Duratuon")
 	}
@@ -81,6 +113,46 @@ func TestMinTagForInt(t *testing.T) {
 	}) {
 		t.Errorf("min tag does not validate for int64")
 	}
+
+	if nil != Validate(struct {
+		field int `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for int")
+	}
+
+	if nil != Validate(struct {
+		field int8 `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for int8")
+	}
+
+	if nil != Validate(struct {
+		field int16 `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for int16")
+	}
+
+	if nil != Validate(struct {
+		field int32 `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for int32")
+	}
+
+	if nil != Validate(struct {
+		field int64 `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for int64")
+	}
 }
 
 func TestMaxTagForInt(t *testing.T) {
@@ -123,6 +195,46 @@ func TestMaxTagForInt(t *testing.T) {
 	}) {
 		t.Errorf("max tag does not validate for int64")
 	}
+
+	if nil != Validate(struct {
+		field int `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for int")
+	}
+
+	if nil != Validate(struct {
+		field int8 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for int8")
+	}
+
+	if nil != Validate(struct {
+		field int16 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for int16")
+	}
+
+	if nil != Validate(struct {
+		field int32 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for int32")
+	}
+
+	if nil != Validate(struct {
+		field int64 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for int64")
+	}
 }
 func TestMinTagForUint(t *testing.T) {
 	if nil == Validate(struct {
@@ -161,6 +273,46 @@ func TestMinTagForUint(t *testing.T) {
 		field uint64 `min:"10"`
 	}{
 		field: 9,
+	}) {
+		t.Errorf("min tag does not validate for uint64")
+	}
+
+	if nil != Validate(struct {
+		field uint `min:"10"`
+	}{
+		field: 10,
+	}) {
+		t.Errorf("min tag does not validate for uint")
+	}
+
+	if nil != Validate(struct {
+		field uint8 `min:"10"`
+	}{
+		field: 10,
+	}) {
+		t.Errorf("min tag does not validate for uint8")
+	}
+
+	if nil != Validate(struct {
+		field uint16 `min:"10"`
+	}{
+		field: 10,
+	}) {
+		t.Errorf("min tag does not validate for uint16")
+	}
+
+	if nil != Validate(struct {
+		field uint32 `min:"10"`
+	}{
+		field: 10,
+	}) {
+		t.Errorf("min tag does not validate for uint32")
+	}
+
+	if nil != Validate(struct {
+		field uint64 `min:"10"`
+	}{
+		field: 10,
 	}) {
 		t.Errorf("min tag does not validate for uint64")
 	}
@@ -206,6 +358,46 @@ func TestMaxTagForUint(t *testing.T) {
 	}) {
 		t.Errorf("max tag does not validate for uint64")
 	}
+
+	if nil != Validate(struct {
+		field uint `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for uint")
+	}
+
+	if nil != Validate(struct {
+		field uint8 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for uint8")
+	}
+
+	if nil != Validate(struct {
+		field uint16 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for uint16")
+	}
+
+	if nil != Validate(struct {
+		field uint32 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for uint32")
+	}
+
+	if nil != Validate(struct {
+		field uint64 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for uint64")
+	}
 }
 
 func TestMinTagForFloat(t *testing.T) {
@@ -221,6 +413,22 @@ func TestMinTagForFloat(t *testing.T) {
 		field float64 `min:"0"`
 	}{
 		field: -0.1,
+	}) {
+		t.Errorf("min tag does not validate for flaot64")
+	}
+
+	if nil != Validate(struct {
+		field float32 `min:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("min tag does not validate for flaot32")
+	}
+
+	if nil != Validate(struct {
+		field float64 `min:"0"`
+	}{
+		field: 0,
 	}) {
 		t.Errorf("min tag does not validate for flaot64")
 	}
@@ -241,5 +449,104 @@ func TestMaxTagForFloat(t *testing.T) {
 		field: 0.1,
 	}) {
 		t.Errorf("max tag does not validate for flaot64")
+	}
+
+	if nil != Validate(struct {
+		field float32 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for flaot32")
+	}
+
+	if nil != Validate(struct {
+		field float64 `max:"0"`
+	}{
+		field: 0,
+	}) {
+		t.Errorf("max tag does not validate for flaot64")
+	}
+}
+
+func TestMinTagForString(t *testing.T) {
+	if nil == Validate(struct {
+		field string `min:"2"`
+	}{
+		field: "a",
+	}) {
+		t.Errorf("min tag does not validate for string")
+	}
+
+	if nil != Validate(struct {
+		field string `min:"2"`
+	}{
+		field: "ab",
+	}) {
+		t.Errorf("min tag does not validate for string")
+	}
+}
+func TestMaxTagForString(t *testing.T) {
+	if nil == Validate(struct {
+		field string `max:"2"`
+	}{
+		field: "abc",
+	}) {
+		t.Errorf("max tag does not validate for string")
+	}
+
+	if nil != Validate(struct {
+		field string `max:"2"`
+	}{
+		field: "ab",
+	}) {
+		t.Errorf("max tag does not validate for string")
+	}
+}
+
+func TestMinTagForMap(t *testing.T) {
+	if nil == Validate(struct {
+		field map[string]string `min:"2"`
+	}{
+		field: map[string]string{
+			"a": "a",
+		},
+	}) {
+		t.Errorf("min tag does not validate for map")
+	}
+
+	if nil != Validate(struct {
+		field map[string]string `min:"2"`
+	}{
+		field: map[string]string{
+			"a": "a",
+			"b": "b",
+		},
+	}) {
+		t.Errorf("min tag does not validate for map")
+	}
+}
+
+func TestMaxForMap(t *testing.T) {
+	if nil == Validate(struct {
+		field map[string]string `max:"2"`
+	}{
+		field: map[string]string{
+			"a": "a",
+			"b": "b",
+			"c": "c",
+		},
+	}) {
+		t.Errorf("max tag does not validate for map")
+	}
+
+	if nil != Validate(struct {
+		field map[string]string `max:"2"`
+	}{
+		field: map[string]string{
+			"a": "a",
+			"b": "b",
+		},
+	}) {
+		t.Errorf("max tag does not validate for map")
 	}
 }
