63b40c63bbbbbc997566ce453261a5a45f71679f
Vadym Myrgorod <vm386@georgetown.edu>
2019-05-31 15:23:47 -0400

    Move functions back to the main file, rename some funcs and add comments

--- validate.go
+++ validate.go
@@ -3,6 +3,7 @@ package validate
 import (
 	"errors"
 	"reflect"
+	"strings"
 )
 
 const (
@@ -36,7 +37,7 @@ func Validate(element interface{}) error {
 	return errors.New("not a struct or a struct pointer")
 }
 
-// Iterate over struct fields
+// validateStruct iterates over struct fields
 func validateStruct(value reflect.Value) error {
 	typ := value.Type()
 	for i := 0; i < typ.NumField(); i++ {
@@ -48,13 +49,13 @@ func validateStruct(value reflect.Value) error {
 	return nil
 }
 
-// Validate struct field
+// validateField validates a struct field
 func validateField(value reflect.Value, field reflect.StructField, isChild bool) error {
 	kind := value.Kind()
 	tag := field.Tag
 
 	// Perform validators
-	valMap := parseValidators(tag)
+	valMap := parseValidateTag(tag)
 	for valType, validator := range valMap {
 		valType := getValidatorType(valType, isChild)
 		var err error
@@ -96,4 +97,70 @@ func validateField(value reflect.Value, field reflect.StructField, isChild bool)
 	}
 
 	return nil
+}
+
+// parseValidateTag parses validate tag into hash map of validators
+func parseValidateTag(tag reflect.StructTag) map[string]string {
+	valMap := make(map[string]string)
+	entries := strings.Split(tag.Get(masterTag), ",")
+	for _, e := range entries {
+		parts := strings.Split(e, "=")
+
+		if len(parts) == 2 {
+			n := strings.TrimSpace(parts[0])
+			v := strings.TrimSpace(parts[1])
+
+			if n != "" {
+				valMap[n] = v
+			}
+		}
+	}
+
+	return valMap
+}
+
+// getValidatorType returns validator type
+func getValidatorType(valType string, child bool) string {
+	var valChildMap = map[string]string{
+		valTypeChildMin:   valTypeMin,
+		valTypeChildMax:   valTypeMax,
+		valTypeChildEmpty: valTypeEmpty,
+		valTypeChildNil:   valTypeNil,
+		valTypeChildOneOf: valTypeOneOf,
+	}
+
+	if child {
+		if valType, ok := valChildMap[valType]; ok {
+			return valType
+		}
+		return ""
+	}
+
+	return valType
+}
+
+// parseTokens splits validator value into tokens
+func parseTokens(str string) []interface{} {
+	if strings.TrimSpace(str) == "" {
+		return nil
+	}
+
+	tokenStrings := strings.Split(str, "|")
+	tokens := make([]interface{}, len(tokenStrings))
+	for i := range tokenStrings {
+		tokens[i] = strings.TrimSpace(tokenStrings[i])
+	}
+
+	return tokens
+}
+
+// isOneOf check if a token is one of tokens
+func isOneOf(token interface{}, tokens []interface{}) bool {
+	for _, t := range tokens {
+		if t == token {
+			return true
+		}
+	}
+
+	return false
 }
